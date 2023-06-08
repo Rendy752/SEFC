cecb7c236cc763eb2a1761e3c9eb88d2ecabb65c
Rendy Pratama <rendypratama752@gmail.com>
2023-06-08 17:05:49 +0700

    html v.2

--- js/profile.js
+++ js/profile.js
@@ -13,3 +13,23 @@ function set(increment){
     }
     number
 }
+
+var slideIndex=1;
+var imgList=document.getElementsByClassName("banner");
+imgList[0].style.display="block";
+function slide(index){
+    slideIndex+=index;
+    if(slideIndex>imgList.length) slideIndex=1
+    else if(slideIndex<1) slideIndex=imgList.length;
+    
+    for(var i=0;i<imgList.length;i++){
+        imgList[i].style.display="none";
+    }
+
+    imgList[slideIndex-1].style.display="block";
+}
+
+
+setInterval(()=>{
+    slide(1);
+},5000)
